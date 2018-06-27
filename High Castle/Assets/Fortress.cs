882e83d1eb58f7c358c4d921dd82adf68b942712
TTula <TTula@users.noreply.github.com>
2018-06-27 13:37:45 +0200

    Fortress Panel
    fortress panel pops up after activation

---
+++ High Castle/Assets/Fortress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fortress : MonoBehaviour {
+
+    public bool isFortressControlledByPlayer;
+
+    private void Start()
+    {
+        isFortressControlledByPlayer = true;
+    }
+}
---
+++ High Castle/Assets/FortressButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FortressButton : MonoBehaviour {
+
+    private Fortress myFortress;
+    private bool isFortressActive = false;
+    private Transform panelChild;
+
+    private void Start()
+    {
+        panelChild = transform.GetChild(0);
+        foreach (Fortress fortress in FindObjectsOfType<Fortress>())
+        {
+            if (fortress.isFortressControlledByPlayer)
+            {
+                myFortress = fortress;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (myFortress.GetComponent<Buildings>().isActivated)
+        {
+            panelChild.gameObject.SetActive(true);
+            isFortressActive = true;
+        } else
+        {
+            panelChild.gameObject.SetActive(false);
+            isFortressActive = false;
+        }
+
+        if (isFortressActive)
+        {
+            GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(myFortress.transform.position);
+        }
+    }
+
+
+}
